a4511b1a56da0459ccd2b3bb6d7252dab506994c
alfonso222 <alfonsorayo222@gmail.com>
2018-02-20 21:58:16 -0500

    second attemp add chat component

---
+++ src/components/startscreen-components/ChatLinkContainer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+//import '../../styles/borders.css'
+
+class ChatLinkContainer extends React.Component {
+  constructor (props) {
+    super();
+    this.state = {
+      urls: []
+    };
+
+
+  };
+
+
+  render () {
+    return(
+      <div>
+        <p>Chat Link</p>
+        <p>Chat Link</p>
+        <p>Chat Link</p>
+        <p>Chat Link</p>
+        <p>Chat Link</p>
+        <p>Chat Link</p>
+      </div>
+    )
+  };
+}
+
+
+export default ChatLinkContainer;
