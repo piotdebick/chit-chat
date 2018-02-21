9e9caafbd67d1fbab28a5d53bda1795afb18ef5d
Piotr <piotr.debicki@cix.csi.cuny.edu>
2018-02-20 21:53:58 -0500

    displays correct user timezone data

--- src/components/message-components/Message.js
+++ src/components/message-components/Message.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import jstz from 'jstz';
+import moment from 'moment-timezone';
 import '../../styles/Message.css'
 
 function Message(props) {
+  const timeZone = jstz.determine().name();
+  const currTime = moment(props.timeStamp).tz(timeZone).format("ddd, MMM Do YYYY, h:mm:ss a");
   return (
       <div className='Message-box'>
         <p className='Message-text'>{props.from + ': ' + props.message}</p>
-        <h4 className='Message-time'>{props.timeStamp}</h4>
+        <h4 className='Message-time'>{currTime}</h4>
       </div>
     )
 }
