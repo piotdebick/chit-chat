5c086a9de1090050e27d7d18ad775f6abf5ec7d1
Piotr <piotr.debicki@cix.csi.cuny.edu>
2018-03-01 00:15:28 -0500

    deleted comment

--- src/components/message-components/MessageContainer.js
+++ src/components/message-components/MessageContainer.js
@@ -12,44 +12,42 @@ class MessageContainer extends React.Component {
       messages: [],
       socket: null
     };
-    //blooming-citadel-29008.herokuapp.com/
   };
 
   componentDidMount() {
-    chrome.tabs.query({active:true}, (tabs) => {
+    chrome.tabs.query({
+      active: true
+    }, (tabs) => {
       const url = tabs[0].url;
       this.setState({
         socket: socketIOClient("https://chit-chat-api.herokuapp.com", {
-          query: 'r_var='+url
+          query: 'r_var=' + url
         })
       });
-      const { socket } = this.state;
-      if(socket) {
+      const {socket} = this.state;
+      if (socket) {
         socket.on('connect', () => {
           console.log('Conected to server');
         });
         socket.on('disconnect', () => {
           console.log('Disconnected from server');
         });
-        socket.on("newMessage", data => this.setState({ messages: [...this.state.messages, data] }));
+        socket.on("newMessage", data => this.setState({
+          messages: [
+            ...this.state.messages,
+            data
+          ]
+        }));
       }
     })
-
-
   };
 
   render() {
-    const { messages, socket } = this.state;
-    return (
-
-      <div className='MessageContainer'>
-        <MessageList messages={messages}></MessageList>
-        <MessageForm username={this.props.username} socket={socket}></MessageForm>
-      </div>
-
-
-
-    );
+    const {messages, socket} = this.state;
+    return (<div className='MessageContainer'>
+      <MessageList messages={messages}></MessageList>
+      <MessageForm username={this.props.username} socket={socket}></MessageForm>
+    </div>);
   }
 }
 
